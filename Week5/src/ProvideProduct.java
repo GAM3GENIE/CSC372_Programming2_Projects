7c5562da70f5daa8a1304a4140528021042c66de
GAM3GENIE <davidlbernardino@gmail.com>
2026-05-14 06:22:02 -0700

    adding week 5s files

---
+++ Week5/src/ProvideProduct.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ProvideProduct {
+    public static int productNumbers(int[] numbers, int index){
+        // base
+        if(index == numbers.length - 1){
+            return numbers[index];
+        }
+        return numbers[index] * productNumbers(numbers, index + 1);
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println("Enter a number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+
+        int product = productNumbers(numbers, 0);
+        System.out.println("The product of your five numbers is: " + product);
+        sc.close();
+    }
+}
